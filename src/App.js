02fe5acdee749ddab2e147479f53ede6f82fabe6
Cameron Nix <camerondeverenix@gmail.com>
2021-03-09 14:02:16 -0500

    updated App.js with correct importsrts and component structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Discover from "./pages/Discover";
-import About from "./pages/About";
-import Search from "./pages/Search";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Main from "./pages/Main";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
 
+import "App.css";
+
 function App() {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Wrapper>
-          <Route exact path="/" component={About} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/discover" component={Discover} />
-          <Route exact path="/search" component={Search} />
-        </Wrapper>
-        <Footer />
-      </div>
-    </Router>
+    <div>
+      <Navbar />
+      <Wrapper>
+        <Header />
+        <Main />
+      </Wrapper>
+      <Footer />
+    </div>
   );
 }
 
